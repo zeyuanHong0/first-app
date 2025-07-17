a5098b256c79f2977599de8192a1d9796d3aada4
Alexander Smith <hongzeyuan0@gmail.com>
2025-07-17 15:20:08 +0800

    一些样式，原生组件的使用

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <Image
+        style={styles.img}
+        source={{
+          uri: 'https://zssxc.ningbowater.com/nbzssApi/files/C06B6AABEDEE411D6DA0B82A2D1C255B.png',
+        }}
+      />
+      <Text style={styles.text}>hello word</Text>
+      <View style={styles.card}>
+        <Text>这是一个卡片</Text>
+      </View>
     </View>
   );
 }
@@ -22,6 +20,26 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  img: {
+    width: 200,
+    height: 200,
+    marginTop: 50,
+    alignSelf: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 100,
+  },
+  card: {
+    marginTop: 20,
+    margin: 30,
+    backgroundColor: '#eee',
+    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+    borderRadius: 20,
+    padding: 20,
   },
 });
 
